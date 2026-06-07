a76bcc2452a06def2c74d5d486f0ea51f8d7cf49
Joel Gustavo Carhuarica Aguilar <joel.carhuarica.a@uni.pe>
2026-06-07 17:13:49 -0500

    implementar helper getSubtreeSize seguro contra punteros nulos

--- Practica_Calificada3-CC232/include/Treap.h
+++ Practica_Calificada3-CC232/include/Treap.h
@@ -21,6 +21,24 @@ public:
         Node(const T& value, std::uint64_t p, Node* par = nullptr)
             : key(value), priority(p), frequency(1), subtree_size(1), parent(par) {}
     };
+
+private:
+    Node* root_{nullptr};
+    std::mt19937_64 rng_;
+
+    int getSubtreeSize(Node* u) const {
+        return u ? u->subtree_size : 0;
+    }
+
+public:
+    Treap() : rng_(232) {}
+    ~Treap() { clear(); }
+
+    void clear() {
+        root_ = nullptr;
+    }
+
+    Node* root() const { return root_; }
 };
 
 } // namespace ods
