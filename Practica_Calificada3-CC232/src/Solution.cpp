db3523cf113a4bffd5b766840b1e794ff33311ee
Joel Gustavo Carhuarica Aguilar <joel.carhuarica.a@uni.pe>
2026-06-09 17:24:55 -0500

    implementar logica posicional directa para Query 3

--- Practica_Calificada3-CC232/src/Solution.cpp
+++ Practica_Calificada3-CC232/src/Solution.cpp
@@ -11,3 +11,13 @@ long long Solution::processQuery2(long long x, int k) {
     int target_index = total_le - k;
     return tree.findKth(target_index);
 }
+
+long long Solution::processQuery3(long long x, int k) {
+    int total_less = tree.countLess(x);
+    int target_index = total_less + k - 1;
+
+    int current_total = tree.root() ? tree.root()->subtree_size : 0;
+    if (target_index >= current_total) return -1;
+
+    return tree.findKth(target_index);
+}
