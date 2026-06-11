62482a2299d0fc8e1e08ad730d652fe2fcb163ce
Joel Gustavo Carhuarica Aguilar <joel.carhuarica.a@uni.pe>
2026-06-11 02:46:01 -0500

    ultima modificacion del Solution.cpp

--- Practica_Calificada3-CC232/src/Solution.cpp
+++ Practica_Calificada3-CC232/src/Solution.cpp
@@ -5,19 +5,33 @@ void Solution::processQuery1(long long x) {
 }
 
 long long Solution::processQuery2(long long x, int k) {
-    int total_le = tree.countLessOrEqual(x);
-    if (total_le < k) return -1;
-
-    int target_index = total_le - k;
-    return tree.findKth(target_index);
+    //buscamos el nodo de partida(el mas grande que sea <=x)
+    auto* curr = tree.findMaxLessOrEqual(x);
+    if (!curr) return -1;
+    
+    //Avanzamos hacia atras a traves de los nodos de los predecesores
+    while (curr) {
+        if (k <= curr->frequency) {
+            return curr->key; // El K-ésimo elemento está contenido en este nodo
+        }
+        k -= curr->frequency;
+        curr = tree.getPredecessor(curr);
+    }
+    return -1; // No se encontraron suficientes elementos
 }
 
 long long Solution::processQuery3(long long x, int k) {
-    int total_less = tree.countLess(x);
-    int target_index = total_less + k - 1;
-
-    int current_total = tree.root() ? tree.root()->subtree_size : 0;
-    if (target_index >= current_total) return -1;
-
-    return tree.findKth(target_index);
+    //buscamos el nodo de partida(el mas pequeño que sea >=x)
+    auto* curr = tree.findMinGreaterOrEqual(x);
+    if (!curr) return -1; 
+    
+    // Avanzamos hacia adelante a través de los sucesores
+    while (curr) {
+        if (k <= curr->frequency) {
+            return curr->key; // El K-ésimo elemento está contenido en este nodo
+        }
+        k -= curr->frequency;
+        curr = tree.getSuccessor(curr);
+    }
+    return -1; // No se encontraron suficientes elementos
 }
