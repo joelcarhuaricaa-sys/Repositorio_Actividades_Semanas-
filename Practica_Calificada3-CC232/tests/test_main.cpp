908bb0733a3eb35182e6d1ff0f03075c235d0b46
Joel Gustavo Carhuarica Aguilar <joel.carhuarica.a@uni.pe>
2026-06-11 02:54:50 -0500

    modificacion ultima de test_main.cpp

--- Practica_Calificada3-CC232/tests/test_main.cpp
+++ Practica_Calificada3-CC232/tests/test_main.cpp
@@ -28,10 +28,37 @@ void test_caso_duplicados_y_ranking() {
     std::cout << "[PASÓ] Prueba de Elementos Duplicados y Concurrencia." << std::endl;
 }
 
+void test_caso_duplicados_complejo() {
+    Solution sol;
+    sol.processQuery1(10);
+    sol.processQuery1(20);
+    sol.processQuery1(20); 
+    sol.processQuery1(20); 
+    sol.processQuery1(30);
+
+    // Query 2: Menores o iguales a 25, ordenados de mayor a menor: [20, 20, 20, 10]
+    assert(sol.processQuery2(25, 1) == 20);
+    assert(sol.processQuery2(25, 2) == 20);
+    assert(sol.processQuery2(25, 3) == 20);
+    assert(sol.processQuery2(25, 4) == 10);
+    assert(sol.processQuery2(25, 5) == -1);
+
+    // Query 3: Mayores o iguales a 15, ordenados de menor a mayor: [20, 20, 20, 30]
+    assert(sol.processQuery3(15, 1) == 20);
+    assert(sol.processQuery3(15, 2) == 20);
+    assert(sol.processQuery3(15, 3) == 20);
+    assert(sol.processQuery3(15, 4) == 30);
+    assert(sol.processQuery3(15, 5) == -1);
+
+    std::cout << "[PASÓ] Prueba Avanzada de Duplicados y Saltos de Nodos" << std::endl;
+
+} 
+
 int main() {
-    std::cout << "Ejecutando Suite de Verificación Obligatoria..." << std::endl;
+    std::cout << "Ejecutando Suite de Verificación Obligatoria" << std::endl;
     test_caso_vacio_y_ausencia();
     test_caso_duplicados_y_ranking();
+    test_caso_duplicados_complejo();
     std::cout << "Todas las pruebas unitarias pasaron con éxito." << std::endl;
     return 0;
 }
